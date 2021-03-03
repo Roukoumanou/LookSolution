b118d5ba1de5c37d75b017019ceaa690a44e02c6
Lookman <roukoumanouamidou@gmail.com>
2021-03-03 16:28:57 +0100

    Initial commit

---
+++ tests/controller/HomeControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Tests\controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerTest extends WebTestCase
+{
+    public function testHomeController()
+    {
+        
+    }
+}
